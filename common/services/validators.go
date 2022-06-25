5cb575b9c2c5228d4c3f4dba2cbf2e26895cc051
v4lproik <rousseau.joel@gmail.com>
2022-06-25 10:52:57 +0200

    Add password validator

--- common/services/validators.go
+++ common/services/validators.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
 	log "go.uber.org/zap"
+	"unicode"
 )
 
 type ValidatorService struct{}
@@ -17,11 +18,50 @@ func ValidateEnum(fl validator.FieldLevel) bool {
 	return value.IsValid()
 }
 
+func isValidPassword(s string) bool {
+	var (
+		hasMinLen  = false
+		hasUpper   = false
+		hasLower   = false
+		hasNumber  = false
+		hasSpecial = false
+	)
+	if len(s) >= 8 {
+		hasMinLen = true
+	}
+	for _, char := range s {
+		switch {
+		case unicode.IsUpper(char):
+			hasUpper = true
+		case unicode.IsLower(char):
+			hasLower = true
+		case unicode.IsNumber(char):
+			hasNumber = true
+		case unicode.IsPunct(char) || unicode.IsSymbol(char):
+			hasSpecial = true
+		}
+	}
+	return hasMinLen && hasUpper && hasLower && hasNumber && hasSpecial
+}
+
+func ValidatePassword(fl validator.FieldLevel) bool {
+	if pwd, ok := fl.Field().Interface().(string); ok {
+		if !isValidPassword(pwd) {
+			return false
+		}
+	}
+	return true
+}
+
 func (v ValidatorService) AddValidators() {
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		err := v.RegisterValidation("enum", ValidateEnum)
 		if err != nil {
-			log.S().Fatalf("custom validators cannot be registered %v", err)
+			log.S().Fatalf("custom validator enum cannot be registered %v", err)
+		}
+		err = v.RegisterValidation("password", ValidatePassword)
+		if err != nil {
+			log.S().Fatalf("custom validator password cannot be registered %v", err)
 		}
 	}
 }
