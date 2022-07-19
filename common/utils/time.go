c87ba66f9e07f5f198fd4e3c3454366ae0ad5631
jrousseau <v4lproik@gmail.com>
2022-07-19 16:26:37 +0200

    index on add-pow-consensus-mechanism: 196d715 Remove error builder

---
+++ common/utils/time.go
@@ -0,0 +1,31 @@
+package services
+
+import "time"
+
+func init() {
+	DefaultTimeService = new(SimpleTimeService)
+}
+
+var DefaultTimeService *SimpleTimeService
+
+type TimeService interface {
+	Now() time.Time
+}
+
+type SimpleTimeService struct{}
+
+func (t *SimpleTimeService) Now() time.Time {
+	return time.Now().UTC()
+}
+
+func (t *SimpleTimeService) Unix() int64 {
+	return t.Now().Unix()
+}
+
+func (t *SimpleTimeService) UnixUint64() uint64 {
+	return uint64(t.Unix())
+}
+
+func (t *SimpleTimeService) Nano() int64 {
+	return t.Now().UnixNano()
+}
